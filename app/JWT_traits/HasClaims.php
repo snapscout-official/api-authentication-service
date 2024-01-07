41971470f601093333e9cb099775dddb8d8aeb2d
g610g <gio.gonzales@carsu.edu.ph>
2024-01-07 12:02:14 +0800

    not tested implemented claims for agency and merchant

--- app/JWT_traits/HasClaims.php
+++ app/JWT_traits/HasClaims.php
@@ -1,5 +1,8 @@
 <?php
 namespace App\JWT_traits;
+
+use App\Models\Role;
+
 trait HasClaims{
     
     public function getJWTIdentifier()
@@ -8,11 +11,29 @@ trait HasClaims{
     }
     public function getJWTCustomClaims()
     {
-        return [
-            'name' => $this->name,
+        $claims = [
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'birth_date' => $this->birth_date,
+            'tin_number' => $this->tin_number,
+            'gender' => $this->gender,
+            'phone_number' => $this->phone_number,
             'email' => $this->email,
-            'role_id' => $this->role_id,
-            ''
+            'role_id' => null,
+            'location'
         ];
+
+        if ($this->role_id === Role::AGENCY){
+            $agency_data = $this->agency;
+            $claims['agency'] = $agency_data;
+            $claims['location'] = $agency_data->location;
+            return $claims;
+        }
+        if ($this->role_id === Role::MERCHANT){
+            $merchant_data = $this->merchant;
+            $claims['merchant'] = $merchant_data;
+            $claims['location'] = $merchant_data->location;
+            return $claims;
+        }
     }
 }
